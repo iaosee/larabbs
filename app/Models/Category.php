9022518555e449ada83c6c0038411da04c263b07
xiaofeng <xiao_f_x@163.com>
2018-05-05 10:28:21 +0800

    update category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+/*
+ php artisan make:scaffold Topic --schema="
+ title:string:index,
+ body:text,
+ user_id:integer:unsigned:index,
+ category_id:integer:unsigned:index,
+ reply_count:integer:unsigned:default(0),
+ view_count:integer:unsigned:default(0),
+ last_reply_user_id:integer:unsigned:default(0),
+ order:integer:unsigned:default(0),
+ excerpt:text,
+ slug:string:nullable"
+*/
 class Category extends Model
 {
     protected $fillable  = [
