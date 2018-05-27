a8db10c3aa2a98a9250acf001bdb3b13c68576fe
xiaofeng <xiao_f_x@163.com>
2018-05-27 21:47:40 +0800

    add font icon

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -7,5 +7,8 @@
         <p>
             rebuild infrastructure, use bootstrap 4 instead of bootstrap 3.
         </p>
+        <div>
+            <i class="icon iconfont icon-all"></i>
+        </div>
     </section>
 @stop
