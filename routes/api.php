8bf7e9b5a37c4b9d08a1126d9c218eafb5809714
xiaofeng <xiao_f_x@163.com>
2018-10-10 17:49:53 +0800

    add socialiteproviders

--- routes/api.php
+++ routes/api.php
@@ -56,7 +56,22 @@ $api->version('v1', [
         $api->post('captchas', 'CaptchasController@store')
             ->name('api.captchas.store');
     });
+/* 
 
+// 微信登录授权 API 
+https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx9c5af9f69a479e42&redirect_uri=http://larabbs.me&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect
+// 获取 access_token
+https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx9c5af9f69a479e42&secret=0a7357d2d64c7cf7318b6533fd0c1732&code=021mvTam0wEt0k1KjMcm0wlvam0mvTaE&grant_type=authorization_code
+// 通过 access_token获取个人信息
+https://api.weixin.qq.com/sns/userinfo?access_token=14_7ldaakR9ip09llIm4JgXv27QmdetFNjRraG1ySc56lLGDp71VTUXsFpdFAfiJuJI8qLbxsZE1prQ8PXQekecPA&openid=oZWeA1kh89fxklY12H57j0GNVy28&lang=zh_CN
+
+
+$accessToken = '14_7ldaakR9ip09llIm4JgXv27QmdetFNjRraG1ySc56lLGDp71VTUXsFpdFAfiJuJI8qLbxsZE1prQ8PXQekecPA';
+$openID = 'oZWeA1kh89fxklY12H57j0GNVy28';
+$driver = Socialite::driver('weixin');
+$driver->setOpenId($openID);
+$oauthUser = $driver->userFromToken($accessToken);
+*/
 });
 
 $api->version('v2', function($api) {
